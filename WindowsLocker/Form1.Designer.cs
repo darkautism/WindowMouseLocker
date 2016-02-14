f22618f6fb43c75d35f4d13b2f2d335e0eb031ba
農至剛 <darkautism@gmail.com>
2016-02-14 19:57:13 +0800

    GJ

---
+++ WindowsLocker/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsLocker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(239, 88);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Target";
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(137, 43);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(86, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Select Process";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(12, 43);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(119, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "DragMe";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button3_MouseUp);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(61, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(162, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Window";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 112);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "WindowMouseLocker";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ WindowsLocker/Form1.cs
@@ -0,0 +1,84 @@
+﻿using EasyFullScreen;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsLocker {
+    public partial class Form1 : Form {
+        IntPtr desktop = user32.GetDesktopWindow();
+        IntPtr target = user32.GetDesktopWindow();
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e) {
+
+            ProcessList p = new ProcessList();
+            p.ShowDialog();
+            if (p.selectProcess == null) return;
+            target = p.selectProcess.MainWindowHandle;
+            StringBuilder WindowText = new StringBuilder(50);
+            user32.GetWindowText(target, WindowText, WindowText.Capacity);
+            StringBuilder ClassName = new StringBuilder(50);
+            user32.GetClassName(target, ClassName, ClassName.Capacity);
+            textBox1.Text = WindowText.ToString() + " (" + ClassName + ")";
+        }
+
+        private void button3_MouseDown(object sender, MouseEventArgs e) {
+            button3.BackColor = Color.Red;
+            timer1.Enabled = true;
+            timer2.Enabled = false;
+        }
+
+        private void button3_MouseUp(object sender, MouseEventArgs e) {
+            button3.BackColor = Control.DefaultBackColor;
+            timer1.Enabled = false;
+            timer2.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+
+            Point pret = new Point();
+            user32.GetCursorPos(ref pret);
+            IntPtr newtarget = user32.WindowFromPoint(pret);
+            if (newtarget != target) {
+                target = newtarget;
+            }
+            StringBuilder WindowText = new StringBuilder(50);
+            user32.GetWindowText(target, WindowText, WindowText.Capacity);
+            StringBuilder ClassName = new StringBuilder(50);
+            user32.GetClassName(target, ClassName, ClassName.Capacity);
+            textBox1.Text = WindowText.ToString() + " (" + ClassName + ")";
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) {
+            IntPtr focus = user32.GetForegroundWindow();
+            if (target != IntPtr.Zero && focus == target) {
+                user32.RECT rect;
+                //user32.GetWindowRect(new System.Runtime.InteropServices.HandleRef(this, target), out rect);
+                //user32.RECT rect2;
+                user32.GetClientRect(target, out rect);
+                Point p = new Point() ;
+                user32.ScreenToClient(target, ref p);
+                rect.Top -= p.Y;
+                rect.Bottom -= p.Y;
+                rect.Left -= p.X;
+                rect.Right -= p.X;
+                Console.WriteLine(rect + "   " + p);
+                user32.ClipCursor(ref rect);
+            } else {
+                StringBuilder WindowText = new StringBuilder(50);
+                user32.GetWindowText(focus, WindowText, WindowText.Capacity);
+                StringBuilder ClassName = new StringBuilder(50);
+                user32.GetClassName(focus, ClassName, ClassName.Capacity);
+                Console.WriteLine("focus: " + WindowText.ToString() + " (" + ClassName + ")" );
+                user32.ClipCursor(IntPtr.Zero);
+            }
+        }
+    }
+}
